c356408f1d61cbc96ea2e04d73c1fea2503507bc
CamiloSinningUN <camilosinning.cs@gmail.com>
2024-04-20 18:44:07 +0200

    better fix to type request

--- project/api/src/index.ts
+++ project/api/src/index.ts
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 dotenv.config({ path: '.env.example' });
 import app from './app';
-import { } from "./shared/custom/global.d";
+// import { } from "./shared/custom/global.d";
 
 app.listen(app.get('port'), () => {
   console.log(`Server is running on port ${app.get('port')}`);
