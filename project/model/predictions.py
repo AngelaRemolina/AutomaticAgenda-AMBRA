e1e88b2a320be20bcf5e4664edbf19ec3d9ba1a0
Angela Remolina <angelasofiaremolinagutierrez@gmail.com>
2024-05-22 19:17:31 +0200

    directory fix

--- project/model/predictions.py
+++ project/model/predictions.py
@@ -3,14 +3,14 @@ import json
 import tensorflow as tf
 import tensorflow_recommenders as tfrs
 
-#--------------- Loading the models -----------
+# --------------- Loading the models -----------
 
 model = tf.keras.models.load_model("./trained_model")
 
 
 # --------------- Loading necessary data ---------------
 
-BATCH_SIZE = 128 
+BATCH_SIZE = 128
 data_activities = {}
 with open("data_activities.json", "r") as f:
     data_activities = json.load(f)
@@ -18,20 +18,34 @@ activities_list = [act["act_id"] for act in data_activities]
 activities = tf.data.Dataset.from_tensor_slices(activities_list)
 
 
-#--------------- Making predictions -----------
+# --------------- Making predictions -----------
 
-# Create a model that takes in raw query features, and
-index = tfrs.layers.factorized_top_k.BruteForce(model.user_model)
-# recommends activities out of the entire activity dataset.
-index.index_from_dataset(
-  tf.data.Dataset.zip((activities.batch(BATCH_SIZE), activities.batch(BATCH_SIZE).map(model.movie_model)))
-)
+
+def predicting(user, top_n=3):
+
+    # Create a model that takes in raw query features, and
+    index = tfrs.layers.factorized_top_k.BruteForce(model.user_model)
+    # recommends activities out of the entire activity dataset.
+    index.index_from_dataset(
+        tf.data.Dataset.zip(
+            (
+                activities.batch(BATCH_SIZE),
+                activities.batch(BATCH_SIZE).map(model.activity_model),
+            )
+        )
+    )
+
+    # Get recommendations.
+    _, titles = index(tf.constant([str(user)]))
+
+    print(f"\nTop {top_n} recommendations for user {user}:")
+    for i, title in enumerate(titles[0, :top_n].numpy()):
+        title = title.decode("utf-8")
+        print(f"{i + 1}. {title}")
+
 
 # Get recommendations.
-user_test = 'U4' # New user example, it does recomend
-_, titles = index(tf.constant([user_test]))
-print(f"\nRecommendations for user {user_test}: {titles[0, :5]}")
 
-user_test = 'U3'
-_, titles = index(tf.constant([user_test]))
-print(f"\nRecommendations for user {user_test}: {titles[0, :5]}")
+predicting("U4")
+
+predicting("U3")
