9442b4535717732cd01aa2fe5c0301773c128bfc
Angela Remolina <angelasofiaremolinagutierrez@gmail.com>
2024-05-23 18:52:59 +0200

    retrain method done

--- project/model/main.py
+++ project/model/main.py
@@ -1,22 +1,17 @@
 # --- Imports ---
 import json
 from typing import Dict, Text
-
 import numpy as np
 import tensorflow as tf
 import tensorflow_recommenders as tfrs
+
 
 # --- Constants ---
 EMBEDDING_DIMENSION = 32  # Higher values will correspond to models that may be more accurate, but will also be slower to fit and more prone to overfitting.
 BATCH_SIZE = 128
 
-# --- Model loading ---
-
-# model = tf.keras.models.load_model("./trained_model")
-
 
 # --- Data handle ---
-
 
 def get_activities():
     data_activities = []
@@ -102,13 +97,22 @@ def predicting(user, top_n=3):
 
 
 def retrain(data_feedback):
-    model = tf.keras.models.load_model("./trained_model")
-    feedback = structure_input(data_feedback)
+
+    update_feedback(data_feedback)
+
+    # creating a new one and training with all data again
+    # this wont work => loss and metrics don't get exported
+    # model = tf.keras.models.load_model("./trained_model")
+
+    model = ActivityModel(retrieval_weight=1.0)
     model.compile(optimizer=tf.keras.optimizers.Adagrad(0.1))
-    model.fit(feedback, epochs=1) # this wont work = loss error
+
+    feedback = structure_input(data_feedback)
+    model.fit(feedback, epochs=3)
+
+    model.retrieval_task = tfrs.tasks.Retrieval()
     model.compile()
     model.save("./trained_model")
-
 
 
 def structure_input(data_feedback):
@@ -128,4 +132,76 @@ def structure_input(data_feedback):
             ],
         }
     )
-    feedback = feedback.shuffle(1).batch(1).cache()
+    feedback = feedback.shuffle(len(data_feedback)).batch(BATCH_SIZE).cache()
+
+
+def get_activities_tensor():
+    # cast activity data into TF tensor
+    activities_list = [act["act_id"] for act in get_activities()]
+    activities = tf.data.Dataset.from_tensor_slices(activities_list)
+    return activities
+
+
+def get_unique_activities_id():
+    unique_activity_ids = set(activity["act_id"] for activity in get_activities())
+    unique_activity_ids = np.array(list(map(str.encode, unique_activity_ids)))
+    return unique_activity_ids
+
+
+def get_unique_user_id():
+    unique_user_ids = set(u["user_id"] for u in get_feedback())
+    return unique_user_ids
+
+
+class ActivityModel(tfrs.models.Model):
+
+    def __init__(self, retrieval_weight: float) -> None:
+
+        super().__init__()
+
+        self.activity_model: tf.keras.layers.Layer = tf.keras.Sequential(
+            [
+                tf.keras.layers.StringLookup(
+                    vocabulary=get_unique_activities_id(), mask_token=None
+                ),
+                tf.keras.layers.Embedding(
+                    len(get_unique_activities_id()) + 1, EMBEDDING_DIMENSION
+                ),
+            ]
+        )
+        self.user_model: tf.keras.layers.Layer = tf.keras.Sequential(
+            [
+                tf.keras.layers.StringLookup(
+                    vocabulary=get_unique_user_id(), mask_token=None
+                ),
+                tf.keras.layers.Embedding(
+                    len(get_unique_user_id()) + 1, EMBEDDING_DIMENSION
+                ),
+            ]
+        )
+
+        self.retrieval_task: tf.keras.layers.Layer = tfrs.tasks.Retrieval(
+            metrics=tfrs.metrics.FactorizedTopK(
+                candidates=get_activities_tensor().batch(128).map(self.activity_model)
+            )
+        )
+
+        self.retrieval_weight = retrieval_weight
+
+    def call(self, features: Dict[Text, tf.Tensor]) -> tf.Tensor:
+        # We pick out the user features and pass them into the user model.
+        user_embeddings = self.user_model(features["user_id"])
+        # And pick out the activity features and pass them into the activity model.
+        activity_embeddings = self.activity_model(features["act_id"])
+
+        return (user_embeddings, activity_embeddings)
+
+    def compute_loss(
+        self, features: Dict[Text, tf.Tensor], training=False
+    ) -> tf.Tensor:
+
+        user_embeddings, activity_embeddings = self(features)
+
+        retrieval_loss = self.retrieval_task(user_embeddings, activity_embeddings)
+
+        return self.retrieval_weight * retrieval_loss
